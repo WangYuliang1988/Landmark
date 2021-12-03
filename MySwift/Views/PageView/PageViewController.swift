f5bd9ab0537da82df7d605785445dfdaef909937
Yuliang Wang <langzi1206@msn.cn>
2021-12-03 17:04:22 +0800

    Complete Lesson: Interfacing with UIKit.

---
+++ MySwift/Views/PageView/PageViewController.swift
@@ -0,0 +1,82 @@
+//
+//  PageViewController.swift
+//  MySwift
+//
+//  Created by origin on 2021/12/3.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View> : UIViewControllerRepresentable {
+    var pages: [Page]
+    @Binding var currentPage: Int
+    
+    // SwiftUI calls this makeCoordinator() method before makeUIViewController(context:), so that
+    // you have access to the coordinator object when configuring your view controller.
+    //
+    // You can use this coordinator to implement common Cocoa patterns, such as delegates, data
+    // sources, and responding to user events via target-action.
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+        )
+        pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [context.coordinator.controllers[currentPage]],
+            direction: .forward,
+            animated: true
+        )
+    }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+        var parent: PageViewController
+        
+        // The coordinator is a good place to store controllers, because the system initializes them
+        // only once, and before you need them to update the view controller.
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else { return nil }
+            
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else { return nil }
+            
+            if index == controllers.count - 1 {
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed,
+                let visibleViewController = pageViewController.viewControllers?.first,
+                let index = controllers.firstIndex(of: visibleViewController) {
+                    parent.currentPage = index
+                
+            }
+        }
+    }
+}
